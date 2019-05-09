94aaa1e95458ff721c86716f9c54318c6c23fb14
tblotkamp <49906005+tblotkamp@users.noreply.github.com>
2019-05-09 11:17:49 +0200

    Add files via upload

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "a) Ujutro";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 84);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(159, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "b) Poslije podne i predvečer ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 107);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "c) Kasno navečer";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1. Kad se osjećate najbolje?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Dalje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 130);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 153);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(12, 176);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(12, 199);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(85, 17);
+            this.radioButton7.TabIndex = 9;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "radioButton7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 236);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 289);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,523 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton4.Visible = false;
+            radioButton5.Visible = false;
+            radioButton6.Visible = false;
+            radioButton7.Visible = false;
+            richTextBox1.Visible = false;
+        }
+
+
+        int korak = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int ee = 0;
+        int f = 0;
+        int g = 0;
+        int skor = 0;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 1;
+            b = 0;
+            c = 0;
+            d = 0;
+            ee = 0;
+            f = 0;
+            g = 0;
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 1;
+            c = 0;
+            d = 0;
+            ee = 0;
+            f = 0;
+            g = 0;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 1;
+            d = 0;
+            ee = 0;
+            f = 0;
+            g = 0;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 1;
+            ee = 0;
+            f = 0;
+            g = 0;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            ee = 1;
+            f = 0;
+            g = 0;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            ee = 0;
+            f = 1;
+            g = 0;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            ee = 0;
+            f = 0;
+            g = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Drugo pitanje
+            
+            if (korak == 0)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 6;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "2. Kad hodate, hodate…";
+                radioButton1.Text = "a) Dosta brzo, dugim koracima";
+                radioButton2.Text = "b) Relativno brzo, sitnim koracima";
+                radioButton3.Text = "c) Ne baš brzo, uspravne glave, gledajući ispred sebe";
+                radioButton4.Text = "d) Ne baš brzo, sagnute glave";
+                radioButton5.Text = "e) Vrlo polako";
+                radioButton4.Visible = true;
+                radioButton5.Visible = true;
+
+
+            }
+            //Treće pitanje
+            else if (korak == 1)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 7;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (ee == 1)
+                {
+                    skor = skor + 1;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "3. Kad pričate s ljudima…";
+                radioButton1.Text = "a) Stojite prekriženih ruku";
+                radioButton2.Text = "b) Imate sklopljene ruke";
+                radioButton3.Text = "c) Jednu ili obje ruke držite na bokovima ";
+                radioButton4.Text = "d) Dodirujete ili gurkate osobu s kojom razgovarate";
+                radioButton5.Text = "e) Igrate se sa svojim uhom, dodirujete bradu ili uvijate kosu";
+            }
+            //Četvrto pitanje
+            else if (korak == 2)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 5;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 7;
+                }
+                else if (ee == 1)
+                {
+                    skor = skor + 6;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "4. Kad se odmarate, sjedite…";
+                radioButton1.Text = "a) Savinutih koljena, noge su opuštene jedna uz drugu";
+                radioButton2.Text = "b) Prekriženih nogu";
+                radioButton3.Text = "c) Ravno ispruženih nogu";
+                radioButton4.Text = "d) Na jednoj nozi savinutoj ispod vas";
+                radioButton5.Visible = false;
+
+            }
+                //Peto pitanje
+            else if (korak == 3)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 1;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "5. Kad vas nešto uistinu zabavlja, reagirate…";
+                radioButton1.Text = "a) Glasnim smijehom";
+                radioButton2.Text = "b) Smijehom, ali ne glasnim";
+                radioButton3.Text = "c) Tihim smijuckanjem";
+                radioButton4.Text = "d) Sramežljivim smiješkom";
+                
+
+            }
+                //Šesto pitanje
+            else if (korak == 4)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 3;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 2;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "6. Kad idete na neko društveno okupljanje…";
+                radioButton1.Text = "a) Glasni ste pri dolasku, tako da vas svatko uoči";
+                radioButton2.Text = "b) Dolazite nezapaženo i gledate uokolo u potrazi za poznatim ljudima";
+                radioButton3.Text = "c) Potpuno ste neprimjetni i pokušavate da vas se ne zapazi";
+                radioButton4.Visible = false;
+            }
+                //Sedmo pitanje
+            else if (korak == 5)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 2;
+                }
+                
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "7. Jako ste se udubili u nešto, vrlo ste zaposleni i netko vas prekida…";
+                radioButton1.Text = "a) Sretni ste zbog prekida";
+                radioButton2.Text = "b) Krajnje ste iznervirani zbog toga";
+                radioButton3.Text = "c) Negdje ste između te dvije krajnosti";
+            }
+                //Osmo pitanje
+            else if (korak == 6)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 4;
+                }
+
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "8. Koja vam je boja najdraža?";
+                radioButton1.Text = "a) Crvena ili narančasta";
+                radioButton2.Text = "b) Crna";
+                radioButton3.Text = "c) Žuta ili svjetloplava";
+                radioButton4.Text = "d) Zelena";
+                radioButton5.Text = "e) Tamnoplava ili ljubičasta";
+                radioButton6.Text = "f) Bijela";
+                radioButton7.Text = "g) Smeđa ili siva";
+                radioButton4.Visible = true;
+                radioButton5.Visible = true;
+                radioButton6.Visible = true;
+                radioButton7.Visible = true;
+
+            }
+                //Deveto pitanje
+            else if (korak == 7)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 7;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 5;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (ee == 1)
+                {
+                    skor = skor + 3;
+                }
+                else if (f == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (g == 1)
+                {
+                    skor = skor + 1;
+                }
+
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "9. Kad ste navečer u krevetu, nekoliko trenutaka prije nego što ćete zaspati…";
+                radioButton1.Text = "a) Ispruženi ste na leđima";
+                radioButton2.Text = "b) Ispruženi ste potrbuške";
+                radioButton3.Text = "c) Lagano ste se savinuli na boku";
+                radioButton4.Text = "d) Glavu ste oslonili na jednu ruku";
+                radioButton5.Text = "e) Glava vam je ispod pokrivača";         
+                radioButton6.Visible = false;
+                radioButton7.Visible = false;
+
+            }
+                //Deseto pitanje
+            else if (korak == 8)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 7;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (ee == 1)
+                {
+                    skor = skor + 1;
+                }
+
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                label1.Text = "10. Često sanjate da…";
+                radioButton1.Text = "a) Padate";
+                radioButton2.Text = "b) Se tučete ili borite";
+                radioButton3.Text = "c) Nešto ili nekoga tražite";
+                radioButton4.Text = "d) Da letite ili lebdite";
+                radioButton5.Text = "e) Uglavnom ne sanjate";
+                radioButton6.Text = "f) Snovi su vam uvijek ugodni";
+                radioButton6.Visible = true;
+                button1.Text = "Konačan rezultat";
+                
+            }
+
+            else if (korak == 9)
+            {
+                if (a == 1)
+                {
+                    skor = skor + 4;
+                }
+                else if (b == 1)
+                {
+                    skor = skor + 2;
+                }
+                else if (c == 1)
+                {
+                    skor = skor + 3;
+                }
+                else if (d == 1)
+                {
+                    skor = skor + 5;
+                }
+                else if (ee == 1)
+                {
+                    skor = skor + 6;
+                }
+                else if (f == 1)
+                {
+                    skor = skor + 1;
+                }
+                else if (g == 1)
+                {
+                    skor = skor + 1;
+                }
+
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                ee = 0;
+                f = 0;
+                g = 0;
+                
+           
+                if (skor > 60)
+                    richTextBox1.Text = "Preko 60 bodova: DOMINANTNOST\n Drugi vas vide kao osobu s kojojm treba “pažljivo postupati”, kao oholu, egocentričnu i izrazito dominantnu osobu. Neki vam se dive i žele biti poput vas, ali vam ne vjeruju uvijek i oklijevaju stupiti s vama u neki dublji odnos.";
+                else if ((skor >= 51) && (skor <= 60))
+                    richTextBox1.Text = "51 do 60 bodova: TEMPERAMENTNOST\n Drugi vas vide kao uzbudljivu osobu, eksplozivnog temperamenta, impulzivnu, kao prirodnog vođu koji brzo donosi odluke, iako ne uvijek ispravne. Smatraju vas hrabrim, avanturistički nastrojenim, kao nekoga tko će se uvijek usuditi pokušati i tko uživa u avanturi. Uživaju biti u vašem društvu zbog uzbudljivosti kojom zračite.";
+                else if ((skor >= 41) && (skor <= 50))
+                    richTextBox1.Text = "41 do 50 bodova: PRAKTIČNOST\n Drugi vas vide kao direktnu, živahnu, šarmantnu, zabavnu, praktičnu i uvijek zanimljivu osobu koja je uvijek u centru pažnje, ali koja je dovoljno uravnotežena da joj to ne udari u glavu. Također vas smatraju ljubaznom, obzirnom osobom punom razumijevanja koja će uvijek razveseliti i pomoći.";
+                else if ((skor >= 31) && (skor <= 40))
+                    richTextBox1.Text = "31 do 40 bodova: ODANOST\n Drugi vas vide kao razumnu, opreznu, brižnu osobu, pametnu, talentiranu, ali skromnu. Niste osoba koja prebrzo sklapa prijateljstva, ali ste izrazito odani onima koji postanu vašim prijateljima i za uzvrat očekujete istu odanost. Oni koji vas stvarno upoznaju, shvaćaju da je potrebno uistinu puno da se vaša vjera u prijatelja pokoleba, ali vam isto tako treba puno da zaboravite, ako se to uistinu i dogodi.";
+                else if ((skor >= 21) && (skor <= 30))
+                    richTextBox1.Text = "21 do 30 bodova: OPREZNOST\n Prijatelji vas smatraju savjesnom i pedantnom osobom. Vide vas kao vrlo opreznu, izrazito brižnu osobu, kao nekoga tko polagano i ustrajno radi. Uistinu bi ih iznenadilo da iznenada reagirate impulzivno, budući da očekuju od vas da sve ispitujete pažljivo iz svakog ugla i tek onda odustanete. Smatraju da je razlog tome vaša oprezna priroda.";
+                else if (skor < 21)
+                    richTextBox1.Text = "Manje od 21 bod: NEODLUČNOST\n Ljudi vas smatraju sramežljivom, nervoznom, neodlučnom osobom, za koju se potrebno brinuti, koja uvijek želi da netko drugi donese odluku i koja se ne želi ni u što miješati. Vi ste za njih vječno uznemirena osoba koja samo vidi nepostojeće probleme. Neki ljudi vas smatraju dosadnima. Samo oni koji vas dobro poznaju znaju da to niste.";
+                button1.Text = "Igraj ponovno";
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                radioButton5.Visible = false;
+                radioButton6.Visible = false;
+                radioButton7.Visible = false;
+
+                richTextBox1.Visible = true;
+            }
+            else if (korak == 10)
+            {
+                Form1 NewForm = new Form1();
+                NewForm.Show();
+                this.Dispose(false);
+            }
+            korak = korak + 1;
+
+
+
+
+        }
+    }
+}
